75c12f28df5b0034addd8f7f60acce4ddd605f50
Thays Oliveira <thaysoliveira14@hotmail.com>
2024-03-23 16:51:00 -0300

    commit inicial

---
+++ src/styles/scripts/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('form-sorteador').addEventListener('submit', function() {
+        let numeroMaximo = document.getElementById('numero-maximo').value
+        numeroMaximo = parseInt(numeroMaximo)
+        let numeroAleatorio = Math.random() * numeroMaximo
+        console.log(numeroAleatorio)
+    })
+})

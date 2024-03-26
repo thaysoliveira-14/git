8bae038728200cd40fd9bf525c8040fb8c79dc4b
Thays Oliveira <thaysoliveira14@hotmail.com>
2024-03-26 15:52:19 -0300

    adicionadas novas configurações

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('form-sorteador').addEventListener('submit', function() {
+    document.getElementById('form-sorteador').addEventListener('submit', function(evento) {
+        evento.preventDefault()
+
         let numeroMaximo = document.getElementById('numero-maximo').value
+
         numeroMaximo = parseInt(numeroMaximo)
+
         let numeroAleatorio = Math.random() * numeroMaximo
-        alert(numeroAleatorio)
+        numeroAleatorio = Math.floor(numeroAleatorio + 1)
+
+        document.getElementById('resultado-valor').innerText = numeroAleatorio
+        document.querySelector('.resultado').style.display = 'block'
     })
 })

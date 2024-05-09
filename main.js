9c7125af57ebb81a7029e18c15a02700107105e8
Thays Oliveira <138609502+thaysoliveira-14@users.noreply.github.com>
2024-05-09 16:06:21 -0300

    adicionando comentários no main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+// Este trecho de código adiciona um ouvinte de evento que é acionado quando o DOM é completamente carregado
 document.addEventListener('DOMContentLoaded', function() {
+
+    // Aqui, estamos selecionando elementos HTML pelo ID usando o método querySelecto e atribuindo esses elementos a variáveis para posterior manipulação
     const avatarElement = document.querySelector('#avatar')
     const nameElement = document.querySelector('#name')
     const usernameElement = document.querySelector('#username')
@@ -7,14 +10,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const followingElement = document.querySelector('#following')
     const linkProfileElement = document.querySelector('#link-profile')
 
+        // Aqui, estamos fazendo uma requisição à API do GitHub para obter informações sobre um usuário específico
         fetch('https://api.github.com/users/thaysoliveira-14')
+            
+            // Esta função é executada quando a promise é resolvida
         .then(function (resposta) {
+             // Verifica se a resposta da requisição é bem-sucedida
             if (!resposta.ok) {
+                // Se a resposta não for bem-sucedida, lançamos um erro com o status da resposta
                 throw new Error (`Requisição da API falhou com o status ${resposta.json()}`)
             }
+            // Se a resposta for bem-sucedida, retornamos o corpo da resposta como JSON
             return resposta.json()
         })
+
+            // Esta função é executada após a conversão da resposta para JSON
         .then(function (json) {
+            // Atualizamos os elementos HTML com as informações obtidas da resposta JSON
             avatarElement.src = json.avatar_url
             nameElement.innerHTML = json.name
             usernameElement.innerHTML = json.login
@@ -23,11 +35,16 @@ document.addEventListener('DOMContentLoaded', function() {
             followingElement.innerHTML = json.following
             linkProfileElement.href = json.html_url
         })
+            
+        // Esta função é executada se houver algum erro durante o processamento da requisição
         .catch(function (error) {
+            // Exibe um alerta ao usuário informando sobre o erro
             alert('Ocorreu um erro: Tente novamente mais tarde', error)
+            // Registra o erro no console para fins de depuração
             console.error('Erro:', error)
         })
         .finally(function () {
+            // Registra uma mensagem no console indicando que a requisição foi finalizada
             console.log('requisição finalizada')
         })
 })

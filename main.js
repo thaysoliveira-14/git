d79ff87453b42e7178667f8006279bb5a144f77f
Thays Oliveira <thaysoliveira14@hotmail.com>
2024-06-26 13:38:00 -0300

    criando exercício em TypeScript

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicaNumeros(x, y) {
+    return x * y;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicaNumeros(5, 2));
+console.log(saudacao('Thays'));
---
+++ main.ts
@@ -0,0 +1,10 @@
+function multiplicaNumeros(x: number, y: number):number {
+    return x * y;
+}
+
+function saudacao(nome: string):string {
+    return `Olá ${nome}`
+}
+
+console.log(multiplicaNumeros(5, 2))
+console.log(saudacao('Thays'))
